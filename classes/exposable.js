648123ae3be99ae5f880dbd7b03eef77a6a32cb5
Ilya Sergeev <isergeev@intermedia.net>
2020-12-14 15:39:52 +0300

    Classes architechure, Exposable

---
+++ classes/exposable.js
@@ -0,0 +1,23 @@
+/**
+ * Abstract class, commonly desinged to be inherited from objects with generated properties
+ * _expose(options) method exports all properties from class expect constructor
+ */
+module.exports = (core) => core.class((utils) => {
+    utils.Exposable = class Exposable {
+        _expose(options) {
+            const {
+                printValue = false
+            } = options;
+
+            const toExpose = Array.prototype.concat.apply(
+                Object.getOwnPropertyNames(this),
+                Object.getOwnPropertyNames(this.constructor.prototype)
+            ).filter(p => p !== 'constructor')
+            .reduce((o, p) => { o[p] = this[p]; return o }, {});
+            
+            if (printValue)
+                console.log(toExpose);
+            return toExpose;
+        }
+    }
+})
--- core.js
+++ core.js
@@ -2,12 +2,20 @@ const Path = require('path');
 
 module.exports = class CatSDKCore {
     get(required, exportFn) {
+        return this._resolveDeps(required, exportFn, this.module)
+    }
+
+    getClass(required, exportFn) {
+        return this._resolveDeps(required, exportFn, this.class)
+    }
+
+    _resolveDeps(required, exportFn, resolveFn) {
         const requires = required instanceof Function;
         const requiredModules = (requires) ? [] : required;
         const exportFunction = ((requires) ? required : exportFn) || (a => a);
 
         const utils = requiredModules.reduce((_utils, moduleName) => {
-            return this.module(moduleName);
+            return resolveFn.call(this, moduleName);
         }, {})
 
         return exportFunction(utils) || utils;
@@ -18,11 +26,12 @@ module.exports = class CatSDKCore {
         return module(this);
     }
 
-    _resolve(...path) {
-        return require(Path.resolve(...path));
+    class(className) {
+        const classModule = this._resolve('classes', className);
+        return classModule(this);
     }
 
-    _resolveClass() {
-
+    _resolve(...path) {
+        return require(Path.resolve(...path));
     }
 }
